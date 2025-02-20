928c308e5452ef184a622f114eadc12970f0cc4f
Kalanyos Bandi <bandi@cigany.com>
2025-02-20 19:32:14 +0100

    TyperX for Uni

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,24 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using TyperX.ViewModels;
+
+namespace TyperX.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+
+        public void InputKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var viewModel = (MainWindowViewModel)DataContext;
+                viewModel.CheckInput();
+            }
+        }
+    }
+}
