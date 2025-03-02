e04dfcbcb32af5de800100d3c1f1dc9333c12472
Nvirs20 <115690291+Nvirs@users.noreply.github.com>
2025-03-02 20:18:21 +0100

    Update App.axaml.cs

--- App.axaml.cs
+++ App.axaml.cs
@@ -20,8 +20,7 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
+           //View model context validation
             DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = new MainWindow
             {
